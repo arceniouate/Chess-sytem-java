67980894c36cc33890b827810fc384e87f44c234
arceniouate <arcenio.uate@gmail.com>
2020-05-03 18:18:52 +0100

    board Aplication

---
+++ src/main/java/aplication/Program.java
@@ -0,0 +1,10 @@
+package aplication;
+
+import boardGame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position p=new Position(1,3);
+        System.out.println(p);
+    }
+}
--- src/main/java/applicarion/Program.java
+++
@@ -1,7 +0,0 @@
-package applicarion;
-
-public class Program {
-    public static void main(String[] args) {
-        System.out.println("ther berst");
-    }
-}
